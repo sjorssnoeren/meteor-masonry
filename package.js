cc8aaddfff0c3c1bff60a59e1e31121c411b2844
Sjors Snoeren <hello@sjors.me>
2016-01-25 09:22:44 +0100

    Updates Masonry version to 4.0.0

--- package.js
+++ package.js
@@ -1,14 +1,13 @@
 Package.describe({
-	name: 'sjors:meteor-masonry',
-	summary: 'Plug & play package of Masonry',
-	version: '0.0.1',
-	git: 'https://github.com/sjorssnoeren/meteor-masonry'
+  name: 'sjors:meteor-masonry',
+  summary: 'Plug & play package of Masonry',
+  version: '0.0.2',
+  git: 'https://github.com/sjorssnoeren/meteor-masonry'
 });
 
 Package.onUse(function (api) {
-	api.versionsFrom('0.9.0');
+  api.versionsFrom('0.9.0');
 
-	api.use('jquery', 'client');
-
-	api.addFiles('lib/dist/masonry.pkgd.js', 'client');
+  api.use('jquery', 'client');
+  api.addFiles('lib/dist/masonry.pkgd.js', 'client');
 });
